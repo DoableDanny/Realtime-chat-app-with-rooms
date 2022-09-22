ba1358a07927bc0f3972618115e9121c9a55313f
EmmanuelMadehin <tobimadehin@gmail.com>
2022-09-22 02:50:40 +0100

    Improved the Join Room functionalty

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,25 +1,24 @@
 import styles from './styles.module.css';
-import { useNavigate } from 'react-router-dom'; // Add this
+import { useNavigate } from 'react-router-dom';
 
 const Home = ({ username, setUsername, room, setRoom, socket }) => {
-  const navigate = useNavigate(); // Add this
+  const navigate = useNavigate();
 
   const joinRoom = () => {
     if (room !== '' && username !== '') {
       socket.emit('join_room', { username, room });
-    }
 
-    // Redirect to /chat
-    navigate('/chat', { replace: true });
+      navigate('/chat', { replace: true });
+    }
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
-        <h1>{`<>DevRooms</>`}</h1>
+        <h1>{`Login`}</h1>
         <input
           className={styles.input}
-          placeholder='Username...'
+          placeholder='Enter your username'
           onChange={(e) => setUsername(e.target.value)}
         />
 
@@ -28,10 +27,10 @@ const Home = ({ username, setUsername, room, setRoom, socket }) => {
           onChange={(e) => setRoom(e.target.value)}
         >
           <option>-- Select Room --</option>
-          <option value='javascript'>JavaScript</option>
-          <option value='node'>Node</option>
-          <option value='express'>Express</option>
-          <option value='react'>React</option>
+          <option value='centurion'>Centurion</option>
+          <option value='orion'>Orion</option>
+          <option value='deutron'>Deutron</option>
+          <option value='galacticon'>Galacticon</option>
         </select>
 
         <button
