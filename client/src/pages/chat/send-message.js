6245edca5fe414fbb7883d76a9b85ce9762c02a6
Min <min.elliot48@gmail.com>
2023-07-28 17:18:05 +1200

    ennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnter

--- client/src/pages/chat/send-message.js
+++ client/src/pages/chat/send-message.js
@@ -1,31 +1,38 @@
-import styles from './styles.module.css';
-import React, { useState } from 'react';
+import styles from './styles.module.css'
+import React, { useState } from 'react'
 
 const SendMessage = ({ socket, username, room }) => {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('')
 
   const sendMessage = () => {
     if (message !== '') {
-      const __createdtime__ = Date.now();
+      const __createdtime__ = Date.now()
       // Send message to server. We can't specify who we send the message to from the frontend. We can only send to server. Server can then send message to rest of users in room
-      socket.emit('send_message', { username, room, message, __createdtime__ });
-      setMessage('');
+      socket.emit('send_message', { username, room, message, __createdtime__ })
+      setMessage('')
     }
-  };
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      sendMessage()
+    }
+  }
 
   return (
     <div className={styles.sendMessageContainer}>
       <input
         className={styles.messageInput}
-        placeholder='Message...'
+        placeholder="Message..."
         onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={handleKeyDown}
         value={message}
       />
-      <button className='btn btn-primary' onClick={sendMessage}>
+      <button className="btn btn-primary" onClick={sendMessage}>
         Send Message
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default SendMessage;
+export default SendMessage
